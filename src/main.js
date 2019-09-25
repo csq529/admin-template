849668c26540dfcdfef7f8432399f2f78f3abfb9
CcuiShiQiang <946081940@qq.com>
2019-09-25 09:45:57 +0800

    install element normalize

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "normalize.css/normalize.css"; // A modern alternative to CSS resets
+
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false;
 
 new Vue({
